226324bbf5d16ec10993563bd956db4401e6b676
Alan Schio <6757777+schirrel@users.noreply.github.com>
2022-06-10 17:39:11 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const intersecting = {}
+function intersection({ callback, name, sentinel }) {
+  const observer = new IntersectionObserver((changes) => {
+    if (
+      changes[0].isIntersecting !== intersecting[name] &&
+      changes[0].boundingClientRect.top > 5
+    ) {
+      callback(changes[0].isIntersecting)
+      intersecting[name] = changes[0].isIntersecting
+      console.log(changes[0].boundingClientRect)
+    }
+  }, {})
+
+  observer.observe(sentinel, {
+    rootMargin: '-20px',
+    threshold: [1],
+  })
+}
+
+const createSentinel = (name) => {
+  const target = document.querySelector(name)
+  const sentinel = document.createElement('div')
+  sentinel.classList.add(`intersection-sentinel__${name.replace('.', '')}`)
+  target.parentNode.insertBefore(sentinel, target)
+  return sentinel
+}
+export function observe({ callback, name }) {
+  const sentinel = createSentinel(name)
+  intersection({
+    callback: callback,
+    name: name,
+    sentinel,
+  })
+}
