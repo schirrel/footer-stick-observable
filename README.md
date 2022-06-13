cd56c01c4c0ca137833e0f4748525a3c362a9857
Alan Schio <alanbschio@gmail.com>
2022-06-13 09:51:17 -0300

    Improve docs, and code, publishing 1.0.1

--- README.md
+++ README.md
@@ -1,28 +1,33 @@
 # footer-stick-observable
+
 A helper to listen to `position:sticky` when used for bottom, when it is stuck or not
 
+## Install
+
+- ` npm install @schirrel/footer-stick-observable`
 
 ### Usage
-```html
-<footer class="my-sticky-footer"> 
-</footer>
 
+Have your position:sticky at bottom identified
+
+```html
+<style>
+  .my-sticky-footer {
+    bottom: 0;
+    position: sticky;
+  }
+</style>
+<footer class="my-sticky-footer"></footer>
 ```
+
+Import the observe functions from the lib
+
+- `import { observe } from '@schirrel/footer-stick-observable'`
 
 ```js
-import { observe } from 'https://cdn.jsdelivr.net/gh/schirrel/footer-stick-observable/index.js'
-
-const myFooter = document.querySelector('.my-sticky-footer')
 observe({
-  name: '.my-sticky-footer',
-  callback: (isIntersecting) => {
-    if(!isIntersecting) {
-      myFooter.classList.add('sticking') 
-    } else {
-      myFooter.classList.remove('sticking') 
-     }
-  }
-})
+  selector: ".my-sticky-footer",
+  callback: ({ isStuck }) => {},
+});
 ```
 
-Simple [codepen](https://codepen.io/schirrel/pen/WNMYXZj?editors=1111)
--- index.js
+++ index.js
@@ -1,35 +1,50 @@
-const intersecting = {}
+const intersecting = {};
+
+const offsetMargin = "-20px";
+const offsetTop = 5;
+
+const normalizeName = (name) =>
+  name.replaceAll(".", "").replaceAll("#", "").replaceAll(">", "");
+
 function intersection({ callback, name, sentinel }) {
   const observer = new IntersectionObserver((changes) => {
+    const changed = changes[0];
+    if (!changed) return;
+
     if (
-      changes[0].isIntersecting !== intersecting[name] &&
-      changes[0].boundingClientRect.top > 5
+      changed.isIntersecting !== intersecting[name] &&
+      changed.boundingClientRect?.top > offsetTop
     ) {
-      callback(changes[0].isIntersecting)
-      intersecting[name] = changes[0].isIntersecting
+      const isStuck = !changed?.isIntersecting;
+
+      callback({ isStuck });
+      intersecting[name] = changed.isIntersecting;
     }
-  }, {})
+  }, {});
 
   observer.observe(sentinel, {
-    rootMargin: '-20px',
+    rootMargin: offsetMargin,
     threshold: [1],
-  })
+  });
 }
 
 const createSentinel = (name) => {
-  const target = document.querySelector(name)
-  const sentinel = document.createElement('div')
-  sentinel.classList.add(`intersection-sentinel__${name.replace('.', '')}`)
-  target.parentNode.insertBefore(sentinel, target)
-  return sentinel
-}
-export function observe({ callback, name }) {
-  const sentinel = createSentinel(name)
+  const target = document.querySelector(name);
+  const sentinel = document.createElement("div");
+  sentinel.classList.add(`intersection-sentinel__${normalizeName(name)}`);
+  target?.parentNode?.insertBefore(sentinel, target);
+  return sentinel;
+};
+
+export function observe({ callback, selector }) {
+  if (!selector) {
+    throw new Error("HTMLElement selector is required");
+  }
+
+  const sentinel = createSentinel(selector);
   intersection({
     callback: callback,
-    name: name,
+    name: selector,
     sentinel,
-  })
+  });
 }
-
-window.observeStickyBottom = observe
