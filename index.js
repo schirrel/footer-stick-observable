b52ea15e46aec3e8e03a662337a68d69e8150ff1
Alan Schio <6757777+schirrel@users.noreply.github.com>
2022-06-22 11:59:32 -0300

    Error handling

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const offsetMargin = "-20px";
 const offsetTop = 5;
 
 const normalizeName = (name) =>
-  name.replaceAll(".", "").replaceAll("#", "").replaceAll(">", "");
+  name?.replaceAll(".", "")?.replaceAll("#", "")?.replaceAll(">", "");
 
-function intersection({ callback, name, sentinel }) {
+const intersection = ({ callback, name, sentinel }) => {
   const observer = new IntersectionObserver((changes) => {
-    const changed = changes[0];
+    const changed = changes?.[0];
     if (!changed) return;
 
     if (
@@ -18,7 +18,7 @@ function intersection({ callback, name, sentinel }) {
       const isStuck = !changed?.isIntersecting;
 
       callback({ isStuck });
-      intersecting[name] = changed.isIntersecting;
+      intersecting[name] = changed?.isIntersecting;
     }
   }, {});
 
@@ -32,6 +32,10 @@ const createSentinel = (name) => {
   const target = document.querySelector(name);
   const sentinel = document.createElement("div");
   sentinel.classList.add(`intersection-sentinel__${normalizeName(name)}`);
+  if(!target?.parentNode) {
+     throw new Error("Can't selector parent from selector");
+  }
+  
   target?.parentNode?.insertBefore(sentinel, target);
   return sentinel;
 };
@@ -40,7 +44,6 @@ export function observe({ callback, selector }) {
   if (!selector) {
     throw new Error("HTMLElement selector is required");
   }
-
   const sentinel = createSentinel(selector);
   intersection({
     callback: callback,
